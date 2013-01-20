eae80e9481101dee80f90875c6db9ecb306e5592
Michael <mi.freudenthaler@gmail.com>
2013-01-20 18:05:20 +0100

    new detail styling

--- components/News/Detail/Component.php
+++ components/News/Detail/Component.php
@@ -5,6 +5,7 @@ class News_Detail_Component extends Kwc_News_Detail_Component
     {
         $ret = parent::getSettings();
         $ret['cssClass'] = 'webStandard';
+        $ret['placeholder']['backLink'] = trlKwfStatic('back to overview');
 //         $ret['extConfig'] = 'Kwf_Component_Abstract_ExtConfig_None';
         return $ret;
     }
